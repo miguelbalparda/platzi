b9b75106c0c69a4c6fe12b48c3eeadd2298a19ae
Miguel Balparda <miguel.balparda@moozo.com.ar>
2017-10-18 16:44:49 -0400

    Primer commit

---
+++ app/code/Platzi/Test/Command/Test.php
@@ -0,0 +1,21 @@
+<?php
+namespace Platzi\Test\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Test extends Command
+{
+    protected function configure()
+    {
+        $this->setName("ps:test");
+        $this->setDescription("A command the programmer was too lazy to enter a description for.");
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Hello World");  
+    }
+} 
